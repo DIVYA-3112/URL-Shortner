86f4a4ddb511ec58af5ee56d964a47c0d8ac890d
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-02-20 14:06:32 +0530

    updated gitignore

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config({path : './backend/.env'});
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port - ${PORT}`);
+});
